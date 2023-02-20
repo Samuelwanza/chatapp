4e3f98ebb983cfe2cdb2495ebbba90c9e9760f37
Samuel wanza <s.wanza@alustudent.com>
2023-02-20 19:45:41 +0200

    update

---
+++ src/components/config.js
@@ -0,0 +1,5 @@
+export default {
+  appId: "23293954628e2e84", //Enter your App ID
+  apiKey: "8850fe69fc8bdcb30b2c583b4beab0d45bd85cd6", //Enter your API KEY
+  GUID: "testgroup", // Enter your group UID
+};
